4389e5e394211586b8d629ececfc20fb2620a0db
Jose Vidal <josevidal1988@live.com>
2019-10-06 22:50:57 -0400

    Mejorando la seccion -about-

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="container">
     <h1 class="title">Nosotros</h1>
+    <hr>
+    <div>
+      <img src="../assets/logo-vidaldev.png" alt="VidalDev">
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
-
+  h1 {
+    margin: 25px 0;
+    color: white
+  }
+  .container div {
+    text-align: center;
+  }
 </style>
