e4a5463a2af4dbe9b923a5135a660874396fc5bb
Jose Vidal <josevidal1988@live.com>
2019-10-06 23:20:35 -0400

    Cambio menores a rutas

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,11 @@ import About from '@/components/About.vue'
 import TrackDetail from '@/components/TrackDetail.vue'
 
 const routes = [
-  { path: '/', component: Search, name: 'search' },
+  {
+    path: '/vidal-music/',
+    component: Search,
+    name: 'search'
+  },
   { path: '/about', component: About, name: 'about' },
   { path: '/track/:id', component: TrackDetail, name: 'track' }
 ]
